e43ea390f759f12ae7a5b70415a87f8c4cc2755b
Roger <47489692+shawroger@users.noreply.github.com>
2019-07-14 17:48:47 +0800

    Update RD_connect .php

--- RD_connect .php
+++ RD_connect .php
@@ -1,43 +1,37 @@
 <?php
-header("Content-type:text/html;charset=utf-8");
 
+header("Content-type:text/html;charset=utf-8");
 class RD
 {
     var $HOST;
     var $USER;
     var $PASS;
     var $CONNECT;
-
-    
+   
 function __construct($HOST,$USER,$PASS,$TABLE){
-
     $this->HOST=$HOST;
-	$this->USER=$USER;
-	$this->PASS=$PASS;
+    $this->USER=$USER;
+    $this->PASS=$PASS;
     $CONNECT=mysqli_connect($HOST,$USER,$PASS);
     mysqli_query($CONNECT,"set names utf8");
-	$this->CONNECT=$CONNECT;
-	mysqli_select_db($this->CONNECT,$TABLE);
+    $this->CONNECT=$CONNECT;
+    mysqli_select_db($this->CONNECT,$TABLE);
 }
 
-
-
 // $SEARCH="SELECT * FROM XX WHERE YY='{$YY}'";
-
 function RD_Getdata($SEARCH){
     $DO_SEARCH=mysqli_query($this->CONNECT,$SEARCH);
     $GET_SEARCH=mysqli_fetch_array($DO_SEARCH, MYSQLI_ASSOC);
     $this->DO_SEARCH=$DO_SEARCH;
     $this->GET_SEARCH=$GET_SEARCH;
-
 }
 
-// $ADD="INSERT INTO label (A,B) VALUES "('{$A}','{$B}')";
-
-// $EDIT="UPDATE label SET A='{$A}' WHERE X='{$X}'";
-
-// $DELETE="DELETE FROM A WHERE X='{$X}'"";
-
+/* 
+$ADD="INSERT INTO label (A,B) VALUES "('{$A}','{$B}')";
+$EDIT="UPDATE label SET A='{$A}' WHERE X='{$X}'";
+$DELETE="DELETE FROM A WHERE X='{$X}'"";
+*/
+	
 function RD_RUN($RUN){
     $DO_RUN=mysqli_query($this->CONNECT,$RUN);
 }
@@ -50,9 +44,8 @@ function RD_Strlen($TABLE){
     while($GET_STRLEN=mysqli_fetch_array($DO_STRLEN,MYSQLI_ASSOC))
     {
         $DO_STRLEN_id++;
-    }return $DO_STRLEN_id;
+    }
+    return $DO_STRLEN_id;
 }
-
-
 }
 ?>
