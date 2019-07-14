5e5ac1d0aaf108cf72d721a9ce9f0c6d16a2041c
Roger <47489692+shawroger@users.noreply.github.com>
2019-07-14 16:36:12 +0800

    Add files via upload

---
+++ RD_connect .php
@@ -0,0 +1,66 @@
+<?php
+header("Content-type:text/html;charset=utf-8");
+
+class RD
+{
+    var $HOST;
+    var $USER;
+    var $PASS;
+    var $CONNECT;
+
+    
+function __construct($HOST,$USER,$PASS,$TABLE){
+
+    $this->HOST=$HOST;
+	$this->USER=$USER;
+	$this->PASS=$PASS;
+    $CONNECT=mysqli_connect($HOST,$USER,$PASS);
+    mysqli_query($CONNECT,"set names utf8");
+	$this->CONNECT=$CONNECT;
+	mysqli_select_db($this->CONNECT,$TABLE);
+}
+
+
+
+// $SEARCH="SELECT * FROM XX WHERE YY='{$YY}'";
+
+function RD_Getdata($SEARCH){
+    $DO_SEARCH=mysqli_query($this->CONNECT,$SEARCH);
+    $GET_SEARCH=mysqli_fetch_array($DO_SEARCH, MYSQLI_ASSOC);
+    $this->DO_SEARCH=$DO_SEARCH;
+    $this->GET_SEARCH=$GET_SEARCH;
+
+}
+
+// $ADD="INSERT INTO label (A,B) VALUES "('{$A}','{$B}')";
+
+function RD_Savedata($ADD){
+    $DO_ADD=mysqli_query($this->CONNECT,$ADD);
+}
+
+// $EDIT="UPDATE label SET A='{$A}' WHERE X='{$X}'";
+
+function RD_Editdata($EDIT){
+    $DO_EDIT=mysqli_query($this->CONNECT,$EDIT);
+}
+
+// $DELETE="DELETE FROM A WHERE X='{$X}'"";
+
+function RD_Deletedata($DELETE){
+    $DO_DELETE=mysqli_query($this->CONNECT,$DELETE);
+}
+
+// count the all nums of a table
+function RD_Strlen($TABLE){
+    $STRLEN="SELECT * FROM {$TABLE}";
+    $DO_STRLEN=mysqli_query($this->CONNECT,$STRLEN);
+    $DO_STRLEN_id=0;
+    while($GET_STRLEN=mysqli_fetch_array($DO_STRLEN,MYSQLI_ASSOC))
+    {
+        $DO_STRLEN_id++;
+    }return $DO_STRLEN_id;
+}
+
+
+}
+?>
